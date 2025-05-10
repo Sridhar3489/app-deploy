ce7152cd407d992bfbfab93395e44c0489739493
SaiSridharParimi <parimi.sai@northeastern.edu>
2025-05-10 13:14:48 -0400

    feat: scaffold apps directory

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,12 @@
+import {client} from "@repo/db/client"
+
+export default async function Home(){
+
+  const user = await client.user.findFirst();
+
+  return (<div>
+    <div>{user?.username}</div>
+    <div>{user?.password}</div>
+  </div>)
+  
+}
